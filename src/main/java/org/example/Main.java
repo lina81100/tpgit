2b71631af15b7adcf836689a5dc8b96eda5895b4
antoniofauci <antonio.fauci@etud.univ-jfc.fr>
2024-10-16 10:17:12 +0200

    commit anto

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+//test anto
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
